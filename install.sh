bcac30645ec2cfb8dad7e6ce23e5181071aaad29
Leon Grösch <leongroesch@gmail.com>
2021-04-20 08:34:06 +0200

    Install script: install relevant software

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
+read -p "Install recomended software [Y/n] " -n 1 -r
+echo    # (optional) move to a new line 
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	sudo apt install cmake make vim tmux
+fi
+
 cp .bashrc ~
 cp .tmux.conf ~
 cp .vimrc ~
